aea48972212f014c7df54e8cd4d3842673909124
Gerasimos01 <acivil@hotmail.gr>
2019-12-20 17:04:27 +0200

    shared material host implemntation. Test passed.

---
+++ ISAAR.MSolve.Tests/FEMpartB/LinearCntRveTest.cs
@@ -0,0 +1,48 @@
+﻿using ISAAR.MSolve.FEM.Elements.SupportiveClasses;
+using ISAAR.MSolve.FEM.Materials;
+using ISAAR.MSolve.LinearAlgebra.Commons;
+using ISAAR.MSolve.LinearAlgebra.Matrices;
+using ISAAR.MSolve.LinearAlgebra.Vectors;
+using ISAAR.MSolve.Materials;
+using ISAAR.MSolve.Materials.Interfaces;
+using ISAAR.MSolve.MultiscaleAnalysis;
+using ISAAR.MSolve.MultiscaleAnalysis.Interfaces;
+using ISAAR.MSolve.Solvers.Direct;
+using Xunit;
+
+namespace ISAAR.MSolve.Tests.FEMpartB
+{
+    public class LinearCntRveTest
+    {
+        [Fact]
+        public static void RunCntRveModel()
+        {
+            // Rve matrix material parameters
+            var solidMaterial = new ElasticMaterial3D()
+            {
+                YoungModulus = 4.00, //GPa
+                PoissonRatio = 0.40,
+            };
+
+            // cnt paramaters
+            // define mechanical properties
+            double youngModulus = 1.0; // 5490; // 
+            double shearModulus = 1.0; // 871; // 
+            double poissonRatio = (youngModulus / (2 * shearModulus)) - 1; //2.15; // 0.034;
+            double area = 1776.65;  // CNT(20,20)-LinearEBE-TBT-L = 10nm
+            double inertiaY = 1058.55;
+            double inertiaZ = 1058.55;
+            double torsionalInertia = 496.38;
+            double effectiveAreaY = area;
+            double effectiveAreaZ = area;
+            // CNT beams material and properties
+            var beamMaterial = new ElasticMaterial3D
+            {
+                YoungModulus = youngModulus,
+                PoissonRatio = poissonRatio,
+            };
+            var beamSection = new BeamSection3D(area, inertiaY, inertiaZ, torsionalInertia, effectiveAreaY, effectiveAreaZ);
+
+        }
+    }
+}
--- ISAAR.MSolve.Tests/FEMpartB/LinearRvesCompositeMaterialTet.cs
+++ ISAAR.MSolve.Tests/FEMpartB/LinearRvesCompositeMaterialTet.cs
@@ -109,6 +109,61 @@ namespace ISAAR.MSolve.Tests.FEMpartB
             Assert.True(BondSlipTest.AreDisplacementsSame(Matrix1.CopyToArray2D(), material4.ConstitutiveMatrix.CopytoArray2D()));
         }
 
+        [Fact]
+        public static void CheckSharedShellMaterialClasses()
+        {
+
+
+            double E_disp = 3.5e9; /*Gpa*/ double ni_disp = 0.4; // stather Poisson
+            var outterMaterial = new ElasticMaterial3DtotalStrain() { PoissonRatio = 0.4, YoungModulus = 3.5e9 }; //pascal
+            var innerMaterial = new ElasticMaterial3DtotalStrain() { PoissonRatio = 0.4, YoungModulus = 3.5e9 }; //pascal
+
+            var Vec1 = Vector.CreateFromArray(new double[3] { 1, 0, 0 });
+            var Vec2 = Vector.CreateFromArray(new double[3] { 0.5, 2, 0 });
+            var strain = new double[3] { 0.01, 0, 0 };
+
+
+            var material3 = new ShellElasticMaterial2Dtransformationb() { YoungModulus = E_disp, PoissonRatio = ni_disp, TangentVectorV1 = new double[3] { Vec1[0], Vec1[1], Vec1[2] }, TangentVectorV2 = new double[3] { Vec2[0], Vec2[1], Vec2[2] } };
+            var Matrix1 = Matrix.CreateZero(3, 3); for (int i1 = 0; i1 < 3; i1++) { for (int i2 = 0; i2 < 3; i2++) { Matrix1[i1, i2] = material3.ConstitutiveMatrix[i1, i2]; } }
+            material3.UpdateMaterial(strain);
+            double[] stressesCheck3 = new double[3] { material3.Stresses[0], material3.Stresses[1], material3.Stresses[2] };
+            double[,] consCheck1 = new double[3, 3];
+            for (int i1 = 0; i1 < 3; i1++) { for (int i2 = 0; i2 < 3; i2++) { consCheck1[i1, i2] = material3.ConstitutiveMatrix[i1, i2]; } }
+
+
+            IdegenerateRVEbuilder homogeneousRveBuilder1 = new CompositeMaterialModeluilderTet(outterMaterial, innerMaterial, 100, 100, 100);
+            var material4 = new Shell2dRVEMaterialHostConst(1,1,1,homogeneousRveBuilder1,
+                model => (new SkylineSolver.Builder()).BuildSolver(model))
+            {
+                TangentVectorV1 = new double[3] { Vec1[0], Vec1[1], Vec1[2] },
+                TangentVectorV2 = new double[3] { Vec2[0], Vec2[1], Vec2[2] }
+            };
+            //var Matrix2 = Matrix.CreateZero(3, 3); for (int i1 = 0; i1 < 3; i1++) { for (int i2 = 0; i2 < 3; i2++) { Matrix2[i1, i2] = material4.ConstitutiveMatrix[i1, i2]; } }
+            //material4.UpdateMaterial(strain);
+            //double[] stressesCheck4 = new double[3] { material4.Stresses[0], material4.Stresses[1], material4.Stresses[2] };
+
+            var clonedMAterial = material4.Clone();
+            clonedMAterial.TangentVectorV1 = new double[3] { Vec1[0], Vec1[1], Vec1[2] };
+            clonedMAterial.TangentVectorV2 = new double[3] { Vec2[0], Vec2[1], Vec2[2] };
+            double[,] consCheck2 = new double[3, 3];
+            for (int i1 = 0; i1 < 3; i1++) { for (int i2 = 0; i2 < 3; i2++) { consCheck2[i1, i2] = clonedMAterial.ConstitutiveMatrix[i1, i2]; } }
+
+            var clonedMAterialB = material4.Clone();
+            clonedMAterialB.TangentVectorV1 = new double[3] { Vec1[0], Vec1[1], Vec1[2] };
+            clonedMAterialB.TangentVectorV2 = new double[3] { Vec2[0], Vec2[1], Vec2[2] };
+            var Matrix2 = Matrix.CreateZero(3, 3); for (int i1 = 0; i1 < 3; i1++) { for (int i2 = 0; i2 < 3; i2++) { Matrix2[i1, i2] = clonedMAterialB.ConstitutiveMatrix[i1, i2]; } }
+
+            var clonedMAterial3 = material4.Clone();
+            clonedMAterial3.TangentVectorV1 = new double[3] { Vec1[0], Vec1[1], Vec1[2] };
+            clonedMAterial3.TangentVectorV2 = new double[3] { Vec2[0], Vec2[1], Vec2[2] };
+            var Matrixc = Matrix.CreateZero(3, 3); for (int i1 = 0; i1 < 3; i1++) { for (int i2 = 0; i2 < 3; i2++) { Matrixc[i1, i2] = clonedMAterial3.ConstitutiveMatrix[i1, i2]; } }
+
+
+            Assert.True(BondSlipTest.AreDisplacementsSame(Matrix2.CopyToArray2D(), consCheck2));
+            Assert.True(BondSlipTest.AreDisplacementsSame(Matrixc.CopyToArray2D(), consCheck2));
+            //Assert.True(BondSlipTest.AreDisplacementsSame(Matrix1.CopyToArray2D(), material4.ConstitutiveMatrix.CopytoArray2D()));
+        }
+
         public static bool AreDisplacementsSame(double[] expectedValues,
             double[] computedValues)
         {
